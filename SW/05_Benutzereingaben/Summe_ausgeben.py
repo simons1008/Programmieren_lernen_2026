3bcdbff7e4d69117b86e0334949cd893d8a48104
simons1008 <einmorgen@tdmail.de>
2025-08-21 16:15:18 +0200

    Ordner SW/05_Benutzereingaben eingerichtet

---
+++ SW/05_Benutzereingaben/Summe_ausgeben.py
@@ -0,0 +1,21 @@
+# Die Summe von zwei Zahlen ausgeben
+
+# Benutzereingaben anfordern
+zahl1 = input("Gib die erste  Zahl ein ")
+zahl2 = input("Gib die zweite Zahl ein ")
+
+# Strings in Dezimalzahlen umwandeln
+zahl1 = float(zahl1)
+zahl2 = float(zahl2)
+
+# Summe ausgeben
+print("Die Summe der beiden Zahlen ist ", zahl1 + zahl2)
+
+# Summe von zwei Strings ausgeben
+
+# Benutzereingaben anfordern
+str1 = input("Gib den ersten  String ein ")
+str2 = input("Gib den zweiten String ein ")
+
+# Summe ausgeben
+print("Die Summe der Strings ist", str1 + str2)
---
+++ SW/05_Benutzereingaben/dictionary_erweitern.py
@@ -0,0 +1,16 @@
+# Das Dictionary englisch_deutsch soll erweitert werden
+
+# Dictionary englisch_deutsch
+englisch_deutsch = {'cat': 'Katze', 'dog': 'Hund', 'cow': 'Kuh', 'bird': 'Vogel'}
+
+# Schlüssel erfragen
+key = input("Gib ein englisches Wort an: ")
+
+# Wert erfragen
+value = input("Gib das passende deutsche Wort an: ")
+
+# Dictionary erweitern
+englisch_deutsch[key] = value
+
+# Erweitertes Dictionary ausgeben
+print(englisch_deutsch)
