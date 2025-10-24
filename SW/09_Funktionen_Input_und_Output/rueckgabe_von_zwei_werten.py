90bea2551284ad06fa84b26f4ceeae70478b39ba
simons1008 <einmorgen@tdmail.de>
2025-10-24 21:10:22 +0200

    Doppeltes Programm entfernt

--- SW/09_Funktionen_Input_und_Output/rueckgabe_von_zwei_werten.py
+++
@@ -1,8 +0,0 @@
-# Rückgabe von zwei Werten
-
-# Funktion gibt die Koordinaten eines Punktes zurück
-def wo_bin_ich() -> tuple[int, int]:
-    # ein externes Gerät liefert die Koordinaten 
-    x = 2
-    y = 4
-    return x, y
