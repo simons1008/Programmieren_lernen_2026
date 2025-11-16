766fa1734a698f81c4e8f80ad1d23266b7f33919
simons1008 <einmorgen@tdmail.de>
2025-11-16 14:46:20 +0100

    Programm überarbeitet

--- SW/11_OOP_Klassen/Fahrrad_Klasse.py
+++ SW/11_OOP_Klassen/Fahrrad_Klasse.py
@@ -15,32 +15,30 @@ class BauplanFahrradKlasse():
         # Kilometer aufaddieren
         self.kmstand = 0
 
+    def klingeln(self, anzahl = 1):
+        print(anzahl * "Bim ")
+
     def fahren(self, km):
         print(self.besitzer, "fährt", km, "km")
         self.kmstand += km
         print("Insgesamt ist", self.besitzer, self.kmstand, "km gefahren")
 
-    def klingeln(self, anzahl = 1):
-        print(anzahl * "Bim ")
-
-    def abstellen(self):
-        print(self.besitzer, "hat sein Fahrrad abgestellt")
-
-    def kilometerstand(self):
-        print("Der Kilometerstand ist", self.kmstand, "km")    
-        
-# Instanz erstellen
-mein_fahrrad = BauplanFahrradKlasse("Martin", "Silber", "Touring", 24)
+# Instanz mein_fahrrad erstellen
+mein_fahrrad = BauplanFahrradKlasse("Martin", "silber", "Touring", 24)
 print(mein_fahrrad.besitzer)
 print(mein_fahrrad.farbe)
 print(mein_fahrrad.typ)
 print(mein_fahrrad.gaenge)
 
-# Methoden aufrufen
+# Instanz leih_fahrrad erstellen
+leih_fahrrad = BauplanFahrradKlasse("Peter", "rot", "Renn", 21)
+
+# Methoden von mein_fahrrad aufrufen
 mein_fahrrad.klingeln(3)
 mein_fahrrad.fahren(15)
 mein_fahrrad.fahren(5)
 mein_fahrrad.klingeln(2)
 mein_fahrrad.fahren(3)
-mein_fahrrad.abstellen()
-mein_fahrrad.kilometerstand()
+
+# Methode von leih_fahrrad aufrufen
+leih_fahrrad.fahren(45)
