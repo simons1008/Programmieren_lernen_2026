2c1e7b95ec06ee09c2b51b6d90da41aab642fe86
simons1008 <einmorgen@tdmail.de>
2025-11-16 15:17:20 +0100

    Programme überarbeitet

--- SW/12_OOP_Vererbung_bei_Klassen/Tier_Klasse.py
+++ SW/12_OOP_Vererbung_bei_Klassen/Tier_Klasse.py
@@ -10,13 +10,13 @@ class Tier():
         self.alter = alter
         self.schlafdauer = 0
 
+    def tut_reden(self, anzahl = 1):
+        print(self.rufname, "sagt: ", anzahl * "miau ")
+
     def tut_schlafen(self, dauer):
         print(self.rufname, "schläft jetzt", dauer , "Minuten ")
         self.schlafdauer += dauer
         print(self.rufname, "Schlafdauer insgesamt:", self.schlafdauer, "Minuten")
-
-    def tut_reden(self, anzahl = 1):
-        print(self.rufname, "sagt: ", anzahl * "miau ")
 
 # Kind-Klasse für Katzen
 class BauplanKatzenKlasse(Tier):
--- SW/12_OOP_Vererbung_bei_Klassen/Zweirad_Klasse.py
+++ SW/12_OOP_Vererbung_bei_Klassen/Zweirad_Klasse.py
@@ -13,19 +13,13 @@ class Zweirad():
         # Kilometer aufaddieren
         self.kmstand = 0
 
+    def klingeln(self, anzahl = 1):
+        print(self.besitzer, "klingelt", anzahl * "Bim ")
+
     def fahren(self, km):
         print(self.besitzer, "fährt", km, "km")
         self.kmstand += km
         print("Insgesamt ist", self.besitzer, self.kmstand, "km gefahren")
-
-    def klingeln(self, anzahl = 1):
-        print(anzahl * "Bim ")
-
-    def abstellen(self):
-        print(self.besitzer, "hat sein Fahrrad abgestellt")
-
-    def kilometerstand(self):
-        print("Der Kilometerstand ist", self.kmstand, "km")      
 
 # Kind-Klasse für Fahrräder
 class BauplanFahrradKlasse(Zweirad):
@@ -46,10 +40,6 @@ class Pedelec(Zweirad):
         # Kapazität in Wattstunden
         self.kapazitaet = kapazitaet
 
-    def abstellen(self):
-        """ Überschreiben der Methode der Eltern-Klasse """
-        print(self.besitzer, "hat sein Pedelec abgestellt")
-
     def aufladen(self):
         print("Der Akku ist ausreichend geladen")
 
@@ -60,6 +50,7 @@ print(mein_fahrrad.besitzer)
 # Instanz deinPedelec erstellen
 dein_pedelec = Pedelec("Hans", "Schwarz", "Touring", 12, 300)
 print(dein_pedelec.besitzer)
+print(dein_pedelec.kapazitaet)
 
 # Methoden aufrufen
 dein_pedelec.fahren(15)
@@ -67,13 +58,4 @@ mein_fahrrad.fahren(16)
 dein_pedelec.fahren(3)
 mein_fahrrad.klingeln(2)
 dein_pedelec.klingeln(3)
-mein_fahrrad.abstellen()
-dein_pedelec.abstellen()
-mein_fahrrad.kilometerstand()
-dein_pedelec.kilometerstand()
 dein_pedelec.aufladen()
-  
-# Ausgabe der Kapazität
-print(dein_pedelec.kapazitaet)
-
-    
